30c587651d7a1e0984328d461de8c66db9250d7d
Abigail Elis <abigailelis.s@hotmail.com>
2025-04-11 19:32:07 -0300

    Crea la entidad Factura y la clase FacturaDAO

--- src/main/java/org/example/entities/Factura.java
+++ src/main/java/org/example/entities/Factura.java
@@ -1,4 +1,34 @@
 package org.example.entities;
 
 public class Factura {
+    private int idFactura;
+    private int idCliente;
+
+    public Factura() {
+    }
+
+    public Factura(int id, int idCliente) {
+        this.idFactura = id;
+        this.idCliente = idCliente;
+    }
+
+    public int getId() {
+        return idFactura;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Factura{" +
+                "id=" + idFactura +
+                ", idCliente=" + idCliente +
+                '}';
+    }
 }
