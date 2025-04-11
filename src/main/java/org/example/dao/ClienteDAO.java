12236e1d77c1443285567ecf4f319048d8486730
Abigail Samanta Elis <abigailelis.s@hotmail.com>
2025-04-11 19:34:10 -0300

    add function of insert client in the ClienteDAO and finalize the entity Cliente

--- src/main/java/org/example/dao/ClienteDAO.java
+++ src/main/java/org/example/dao/ClienteDAO.java
@@ -1,4 +1,33 @@
 package org.example.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class ClienteDAO {
+
+    private Connection connection;
+
+    public ClienteDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void addCliente(Cliente cliente) throws SQLException {
+        String query = "INSERT INTO cliente (idCliente, nombre, email) VALUES (?, ?, ?)";
+        PreparedStatement ps = null;
+
+        ps = connection.prepareStatment(query);
+        ps.setInt(1, cliente.getIdCliente());
+        ps.setString(2, cliente.getNombre());
+        ps.setString(3, cliente.getEmail());
+        ps.executeUpdate();
+
+        System.out.println("Cliente insertado exitosamente.");
+
+        if (ps != null) {
+            ps.close();
+        }
+        connection.commit();
+
+    }
+
 }
