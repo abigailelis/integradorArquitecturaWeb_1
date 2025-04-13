ae637e3a5ba86ec5030877179d0a3df15b5739ba
Abigail Elis <abigailelis.s@hotmail.com>
2025-04-13 14:33:22 -0300

    Corrige ortografía del main y borra las instancias de facturaDAO y facturaProductoDAO que eran innecesarias

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -22,26 +22,31 @@ public class Main {
         AbstractFactory chosenFactory = AbstractFactory.getDAOFactory(1);
         ProductoDAO producto = chosenFactory.getProductoDAO();
         ClienteDAO cliente = chosenFactory.getClienteDAO();
-        FacturaDAO factura = chosenFactory.getFacturaDAO();
-        FacturaProductoDAO facturaProducto = chosenFactory.getFacturaProductoDAO();
 
-        //Obtener lista de clientes con mayor facturación
+
+        /**
+         * Obtiene una lista ordenada de clientes bajo el criterio de mayor a menor facturación
+         */
         List<ClienteDTO> clientesDTO = cliente.getClientesMayorFacturacion();
 
-        System.out.println("\n\nLista de clientes ordenados por mayor facturacion:");
+        System.out.println("\n\nLista de clientes ordenados por mayor facturacion: ");
 
         for(ClienteDTO clienteDTO : clientesDTO){
             System.out.println(clienteDTO);
         }
+
         System.out.println("\nCantidad de clientes encontrados: " + clientesDTO.size());
 
-        //obtener el producto que mas recuado
+        /**
+         * Obtiene el producto que más recaudó, calculando la cantidad * el valor del mismo
+         * en la totalidad de las facturas en las que se encuentre
+         */
         ProductoDTO productoDTO = producto.getProductoMayorRecaudacion();
 
         if (productoDTO == null) {
-            System.out.println("\n\nNo se encontro el producto");
+            System.out.println("\n\n¡No se encontró el producto!");
         }else {
-            System.out.println("\n\nEl producto que mas recaudo es " + productoDTO);
+            System.out.println("\n\nEl producto que mas recaudó es: " + productoDTO);
         }
 
     }
